d62f0081896c847034199edb014b7ccc9f490242
PatcharapholToopprasom <bignorth46@gmail.com>
2025-12-16 17:01:01 +0700

    feat: Add NestJS Config module and initialize main application module with core dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { OntologyModule } from './ontology/ontology.module';
 import { HyperlinkModule } from './hyperlink/hyperlink.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, OntologyModule, HyperlinkModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),PrismaModule, OntologyModule, HyperlinkModule],
   controllers: [AppController],
   providers: [AppService],
 })
