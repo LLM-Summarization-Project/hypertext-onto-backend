bf11bfdd8c7a9fb144245dd5eb0b448b91fb72a8
tirenton <it9026@gmail.com>
2026-01-28 04:02:49 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,15 @@ async function bootstrap() {
     ],
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Hypertext Backend')
+    .setDescription('The Hypertext Backend API description')
+    .setVersion('1.0')
+    .build();
+  const document = await SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3001, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
